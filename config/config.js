d90f2fe27c955387771890877d79d8fc87c2be47
jasalguero <jasalguero@gmail.com>
2014-05-14 22:25:58 +0200

    fixed production json

--- config/config.js
+++ config/config.js
@@ -4,5 +4,5 @@ var _ = require('underscore'),
 // Load app configuration
 
 module.exports = _.extend(
-  // require(__dirname + '/../config/env/all.js'),
+  require(__dirname + '/../config/env/all.js'),
   require(__dirname + '/../config/env/' + env + '.json') || {});
